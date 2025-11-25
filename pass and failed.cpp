38fb3aaa42ce8d2e98e614a76efc1eccd7fc59ca
marojutaruni2-blip <marojutaruni2@gmail.com>
2025-11-25 10:28:40 +0530

    PASS OR FAILED

---
+++ pass and failed.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+ main()
+{
+	int m;
+	printf("enter m marks");
+	scanf("%d",&m);
+	if(m>=35)
+{
+	if(m>=75)
+	{
+		printf("the student is passed in distinction");
+	}
+	else
+	{
+		printf("the student is passed");
+     }
+}
+else
+{
+	printf("the student is failed");
+}
+}
