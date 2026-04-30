09f39ea87d2f0c24d86040e1157d7ec8eb14f3f5
marojutaruni2-blip <marojutaruni2@gmail.com>
2026-04-30 10:28:32 +0530

    lambda

---
+++ lambda functions.py
@@ -0,0 +1,13 @@
+#lambda functions:these are one line functions
+#syntax:lambda keyword parameters:expression(or)operation
+#example:
+c=lambda a,b:a+b
+print(c)
+
+
+#module
+calc.py
+def  add(a,b):
+    return a+b
+def  multiply(a,b):
+    return a*b
