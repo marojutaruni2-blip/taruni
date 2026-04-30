97714c06b05693beb93dd8447d4e1f2ebcfbe467
marojutaruni2-blip <marojutaruni2@gmail.com>
2026-04-30 10:26:29 +0530

    tuple

---
+++ tuple&dict.py
@@ -0,0 +1,12 @@
+tuple = ("taruni","lahari","gayatri","durga")
+print(tuple)
+print(tuple[0])
+print(tuple[1])
+print(tuple[-1])
+print(tuple[0:1])
+
+
+dict={}
+print(dict)
+dict[0]="name"
+print(dict)
