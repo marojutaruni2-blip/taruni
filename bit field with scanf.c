299179f989271696fe3793688276a2955e3d9ed0
marojutaruni2-blip <marojutaruni2@gmail.com>
2026-05-11 20:48:24 +0530

    Add files via upload

---
+++ bit field with scanf.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+struct demo
+{
+unsigned int a:5;
+unsigned int b:6;
+unsigned int c:6;
+};
+int main(){
+	struct demo v;
+	v.a=19;
+	v.b=25;
+	v.c=56;
+	printf(" enter the nit fields:");
+	scanf("\n %d \n %d \n %d",&v.a,&v.b,&v.c);
+	return 0;
+}
