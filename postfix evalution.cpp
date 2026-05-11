6d11f844254abdc38b083165e5857750c1de92d3
marojutaruni2-blip <marojutaruni2@gmail.com>
2026-05-11 20:46:39 +0530

    Add files via upload

---
+++ postfix evalution.cpp
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<ctype.h>
+int precedence(char op)
+{
+	if(op=='^')
+	return 3;
+	if(op=='*'|| op=='/')
+	return 2;
+	if(op=='+'||op=='-')
+	return 1;
+	return 0;
+}
+int main()
+{
+	char infix[50],stack[50],postfix[50];
+	int i=0;j=0;top=-1;
+	printf("\n enter the infix position")
+	printf("%s",infix);
+	while(infix[i]!='10')
+	{
+		if(isalnum(infix[i]))
+	{
+		postfix[j]=infix[i];
+		j++
+		}
+		else if(infix[i]=='(')	
+		{
+			top++;
+			stack[top]=infix[i];
+		}
+		else if(infix[i]==')')
+		{
+			while(stack[top]!='(')
+			{
+				postfix[j]=stack[top];
+				top--;j++;
+			}
+			top--;
+		}
+		else
+		{
+			while(top!=-1&&(precedence(stack[top]>precedence(infix[i])||(precedence(stack[top]==precedence(infix[i])&&infix[i])))))
+			postfix[j]=stack[top];
+			top--;
+			j++;
+		}
+}
+}
