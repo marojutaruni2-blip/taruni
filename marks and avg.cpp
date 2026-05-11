8d4cc42a9ea5086c8d6ee24dcdadba7da2b26b34
marojutaruni2-blip <marojutaruni2@gmail.com>
2026-05-11 20:31:52 +0530

    Add files via upload

---
+++ marks and avg.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+struct student
+{
+	int marks;
+};
+int main()
+{
+	struct student s[100];
+	int n,i,total=0;
+	float avg;
+	printf("\n enter n size");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		printf("\n enter student %d marks",i+1);
+		scanf("%d",&s[i].marks);
+		total=total+s[i].marks;
+	}
+	avg=(float)total/n;
+	printf("\n total marks =%d \n average=%f",total,avg);
+	return 0;
+}
+
