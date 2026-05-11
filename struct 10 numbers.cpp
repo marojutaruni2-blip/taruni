f1c4d3a311e7c3fe961bbc575e604bc9bdd92440
marojutaruni2-blip <marojutaruni2@gmail.com>
2026-05-11 20:30:45 +0530

    Add files via upload

---
+++ struct 10 numbers.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+typedef struct
+{
+	int num[10];
+}numbers;
+int main()
+{
+ numbers n;
+int i;
+printf("enter 10 numbers");
+for(i=0;i<10;i++)
+{
+scanf("%d",&n.num[i]);
+}
+return 0;
+}
+
